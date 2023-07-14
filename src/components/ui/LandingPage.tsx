4fd42a5600ea8a1d1d408909f927370d05e8cf7e
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-07-15 02:08:46 +0600

    [Done] Data Fetch -->home page -- using RTQ query

--- src/components/ui/LandingPage.tsx
+++ src/components/ui/LandingPage.tsx
@@ -5,11 +5,20 @@ interface IProps {
 export default function LandingPage({ book }: IProps) {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">{book?.title}</h2>
-        {/* <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button> */}
+      <div className="card-body items-center text-center">
+        <h2 className="card-title text-2xl text-pink-500">{book?.title}</h2>
+        <div className="text-base font-normal">
+          <p>
+            <span className=" text-lg ">Author : </span> {book?.author}
+          </p>
+          <p>
+            <span className=" text-lg ">Genre: </span> {book?.genre}
+          </p>
+          <p>
+            <span className=" text-lg ">PublicationDate: </span>
+            {book?.publicationDate}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,20 @@
 import LandingPage from "../components/ui/LandingPage";
+import { useGetLandingPageBooksQuery } from "../redux/api/apiSlice";
 import { IBooks } from "../types/globalTypes";
 
 export default function Home() {
-  const datacollection = [
-    {
-      title: "Baiser Bonna",
-      author: "Tasrif Khan",
-      genre: "Contemporary Stories",
-      status: true,
-      publicationDate: "22 Feb 2023",
-      review: [],
-    },
-    {
-      title: "Pyara Hin 2",
-      author: "Antiq Mahmud",
-      genre: "Comic Book",
-      status: true,
-      publicationDate: "02 Aug 2022",
-      review: [],
-    },
-  ];
+  //data fetch using RTK Query
+  const { data } = useGetLandingPageBooksQuery(undefined);
+  console.log(data);
+  let bookData = data?.data;
+
   return (
     <div className="my-10">
       <h3 className="text-center text-xl font-semibold">
-        Recently Added Books
+        Top 10 Recently Added Books
         <div className="col-span-9 grid grid-cols-3 gap-10 pb-20 mt-10">
-          {datacollection?.map((book: IBooks) => (
-            <LandingPage book={book} />
+          {bookData?.map((book: IBooks) => (
+            <LandingPage book={book} key={book.title} />
           ))}
         </div>
       </h3>
