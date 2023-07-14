ae78be152b9e002cacb2bd9b3eb69a3ed116e8d2
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-07-15 00:42:14 +0600

    [Done] create Api Slice & Modify store for it

--- src/redux/api/store.ts
+++ src/redux/api/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./apiSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 // for typescript type declaretion
